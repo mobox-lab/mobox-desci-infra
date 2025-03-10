ac8198b9b5fa0812c297932ffcacbc052c4976cf
joji-mb <545633740@qq.com>
2025-03-10 20:34:44 +0100

    feat(ui): add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/common/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Research from './pages/Research'
+import Lab from './pages/Lab'
+import Community from './pages/Community'
+import Marketplace from './pages/Marketplace'
+import Economics from './pages/Economics'
+import Documentation from './pages/Documentation'
+import Support from './pages/Support'
+import Settings from './pages/Settings'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="research" element={<Research />} />
+        <Route path="lab" element={<Lab />} />
+        <Route path="community" element={<Community />} />
+        <Route path="marketplace" element={<Marketplace />} />
+        <Route path="economics" element={<Economics />} />
+        <Route path="documentation" element={<Documentation />} />
+        <Route path="support" element={<Support />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
