d5cbf3ea366bb9361df3ddac7b58e5ffdbabf819
C-Green-Code <shallowgreenjob@gmail.com>
2025-06-29 17:36:00 +0200

    perf(api): enhance functionality

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import  { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'  // Minor update
 
 export default defineConfig({
-  plugins: [react()], 
+ plugins: [react()], 
   server: {  // Minor update
     port: 3000,
     open: true,
