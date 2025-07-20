9f60a3674bf1af35cb4860182d16ca8e86ffad94
Goro  Yagami <goro@mobox.io>
2025-07-20 13:01:22 +0200

    docs(ui): update documentation

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import './styles/index.css'
+import './styles/index.css'  // Minor update
   // Minor update
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
